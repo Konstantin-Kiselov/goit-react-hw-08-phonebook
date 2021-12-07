af94e1b89491934c87d773d7ded575352cfcbfb6
Konstantin-Kiselov <constantine.kiss@gmail.com>
2021-12-08 00:45:48 +0200

    add persist and autharization

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import {
+  persistStore,
+  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
@@ -7,8 +9,10 @@ import {
   PURGE,
   REGISTER,
 } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 import contactsReducer from './contacts/contacts-reducer';
+import { authReducer } from '../redux/auth';
 
 const myMiddleware = store => next => action => {
   // console.log('Моя прослойка action', action);
@@ -28,10 +32,19 @@ const middleware = [
   logger,
 ];
 
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  // whitelist: ['token'],
+};
+
 export const store = configureStore({
   reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
     contacts: contactsReducer,
   },
   middleware,
   devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
